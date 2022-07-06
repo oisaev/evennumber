1c68589e0d6daf72dfce81d95358d2462ae8223c
Oleg Isaev <oleg.isayev@gmail.com>
2022-07-06 12:46:32 +0300

    добавил summary

--- Program.cs
+++ Program.cs
@@ -11,21 +11,42 @@ namespace evennumber
                 Console.WriteLine(i.ToString() + " " + Method6(i).ToString());
         }
 
+        /// <summary>
+        /// Проверкой остатка от деления на 2
+        /// </summary>
+        /// <param name="inpNumber"></param>
+        /// <returns></returns>
         static bool Method1(int inpNumber)
         {
             return inpNumber % 2 == 0;
         }
 
+        /// <summary>
+        /// Проверкой, является ли последняя цифра в числе четной
+        /// </summary>
+        /// <param name="inpNumber"></param>
+        /// <returns></returns>
         static bool Method2(int inpNumber)
         {
             return "02468".Contains(inpNumber.ToString()[^1]);
         }
 
+        /// <summary>
+        /// Использованием побитового И для определения младшего разряда в бинарной записи числа
+        /// (в нечетном числе всегда 1)
+        /// </summary>
+        /// <param name="inpNumber"></param>
+        /// <returns></returns>
         static bool Method3(int inpNumber)
         {
             return ! Convert.ToBoolean(inpNumber & 1);
         }
 
+        /// <summary>
+        /// То же, что и метод 2, но в записи с использованием регулярного выражения
+        /// </summary>
+        /// <param name="inpNumber"></param>
+        /// <returns></returns>
         static bool Method4(int inpNumber)
         {
             string input = inpNumber.ToString();
@@ -33,11 +54,22 @@ namespace evennumber
             return Regex.Match(input, pattern).Success;
         }
 
+        /// <summary>
+        /// Побитовым сдвигом на 31 бит влево и проверкой последнего бита
+        /// (в четном всегда 0)
+        /// </summary>
+        /// <param name="inpNumber"></param>
+        /// <returns></returns>
         static bool Method5(int inpNumber)
         {
             return inpNumber << 31 == 0;
         }
 
+        /// <summary>
+        /// Использование поведения целочисленного деления
+        /// </summary>
+        /// <param name="inpNumber"></param>
+        /// <returns></returns>
         static bool Method6(int inpNumber)
         {
             return inpNumber / 2 * 2 == inpNumber;
